b34baef1d2c8cd91e4fdaef014d140a0c8a3461e
Zuhaib Ahmad <contact@zuhaibahmad.com>
2018-10-17 15:57:04 +0500

    Restructure for multiple demos

--- app-demo/src/main/java/co/sodalabs/demo/DemoActivity.kt
+++ app-demo/src/main/java/co/sodalabs/demo/DemoActivity.kt
@@ -1,17 +1,49 @@
 package co.sodalabs.demo
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.activity_demo.rvDemoList
+
+data class DemoViewModel(val name: String, val description: String, val intent: Intent)
 
 class DemoActivity : AppCompatActivity() {
 
     private val mDisposablesOnCreate = CompositeDisposable()
 
+    private val demoList by lazy {
+        listOf(
+            DemoViewModel(getString(R.string.demo_label_recyclerview), getString(R.string.demo_desc_recyclerview), Intent(this, StyledRecyclerViewDemo::class.java)),
+            DemoViewModel(getString(R.string.demo_label_switch), getString(R.string.demo_desc_switch), Intent(this, StyledSwitchDemo::class.java))
+        )
+    }
+
+    private val demoAdapter by lazy { DemoAdapter(demoList) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo)
 
-        setContentView(R.layout.activity_demo_styled_recycler_view)
+        setupRecyclerView()
+
+    }
+
+    private fun setupRecyclerView() {
+        rvDemoList.apply {
+            adapter = demoAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
+
+        demoAdapter.clickListener = { _, item ->
+            startActivity(item.intent)
+        }
     }
 
     override fun onDestroy() {
@@ -19,5 +51,36 @@ class DemoActivity : AppCompatActivity() {
 
         // Unbind view.
         mDisposablesOnCreate.clear()
+    }
+}
+
+class DemoAdapter(private val items: List<DemoViewModel>) : RecyclerView.Adapter<DemoAdapter.ViewHolder>() {
+
+    var clickListener: ((view: View, item: DemoViewModel) -> Unit)? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.demo_recycler_view_item, parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+        holder.setOnClickListener(clickListener)
+
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        private var clickListener: ((view: View, item: DemoViewModel) -> Unit)? = null
+
+        fun bind(viewModel: DemoViewModel) {
+            itemView.findViewById<TextView>(R.id.tvLabel).text = viewModel.name
+            itemView.findViewById<TextView>(R.id.tvDescription).text = viewModel.description
+            itemView.setOnClickListener { clickListener?.invoke(itemView, viewModel) }
+        }
+
+        fun setOnClickListener(listener: ((view: View, item: DemoViewModel) -> Unit)?) {
+            this.clickListener = listener
+        }
     }
 }
---
+++ styled-switch-view/src/main/java/co/sodalabs/view/StyledSwitch.kt
@@ -0,0 +1,11 @@
+package co.sodalabs.view
+
+import android.content.Context
+import android.support.v7.widget.SwitchCompat
+import android.util.AttributeSet
+
+class StyledSwitch constructor(
+    context: Context,
+    attrs: AttributeSet,
+    defStyleAttr: Int
+) : SwitchCompat(context, attrs, defStyleAttr)
