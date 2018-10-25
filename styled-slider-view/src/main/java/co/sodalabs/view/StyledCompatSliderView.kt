64906b0ea74397f07d1a8100749a4ef07f9c302a
Boy Wang <boyw165@gmail.com>
2018-10-25 22:33:57 +0800

    Fix a not-found reference (#19)

--- styled-slider-view/src/main/java/co/sodalabs/view/StyledCompatSliderView.kt
+++ styled-slider-view/src/main/java/co/sodalabs/view/StyledCompatSliderView.kt
@@ -8,7 +8,6 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.widget.AppCompatSeekBar
 import android.util.AttributeSet
 
-
 /**
  * This is a wrapper of [AppCompatSeekBar] which uses "android:seekBarStyle"
  * style attribute by default and this enable us the hot-theme function.
@@ -39,7 +38,8 @@ class StyledCompatSliderView : AppCompatSeekBar {
         val attributes = context.theme.obtainStyledAttributes(attrs, R.styleable.StyledSlider, 0, 0)
         for (i in 0 until attributes.indexCount) {
             when (attributes.getIndex(i)) {
-                R.styleable.StyledSlider_android_progressDrawable -> progressDrawable = attributes.getDrawable(R.styleable.StyledSlider_android_progressDrawable)
+                R.styleable.StyledSlider_android_progressDrawable -> progressDrawable = attributes.getDrawable(
+                    R.styleable.StyledSlider_android_progressDrawable)
             }
         }
     }
@@ -47,7 +47,7 @@ class StyledCompatSliderView : AppCompatSeekBar {
     override fun onDraw(c: Canvas) {
         super.onDraw(c)
         if (progress >= MINIMUM_PROGRESS) {
-            val available = width - mPaddingRight - thumbBitmap.width
+            val available = width - paddingRight - thumbBitmap.width
             val thumbX = (available * progress / max).toFloat() - thumbBitmap.width
             c.drawBitmap(thumbBitmap, thumbX, 0f, paint)
         }
